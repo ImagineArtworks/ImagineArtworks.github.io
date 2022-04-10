2c59c145087a517b752bcbaab0501f6b9d22fa5c
Imagine <91915381+ImagineArtworks@users.noreply.github.com>
2022-04-10 20:18:18 +0545

    Some edits
    LMFAO forgot to remove debugging code

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 function toggleList() {
-    console.log("here");
     $(".nav-bar").toggleClass("active");
-    console.log(document.querySelector(".nav-bar"));
 }
 
 $(".burger").on("click", toggleList);
